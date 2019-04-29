6c5ab3b586f0d6b247b61fde4e8142b5c2aa58cd
devsda <mail2jhamb@gmail.com>
2019-04-29 21:47:04 +0530

    Added Protocol and UT (#3)
    Added Protocol and UT

--- src/main/java/com/devsda/utils/httputils/constants/Protocol.java
+++ src/main/java/com/devsda/utils/httputils/constants/Protocol.java
@@ -14,4 +14,22 @@ public enum Protocol {
     public String getProtocol() {
         return protocol;
     }
+
+    public static Protocol getProtocol(String protocol) {
+
+        if ("https".equalsIgnoreCase(protocol)){
+            return Protocol.HTTPS;
+        } else if ("http".equalsIgnoreCase(protocol)) {
+            return Protocol.HTTP;
+        } else {
+            throw new IllegalArgumentException(String.format("Wrong protocol value : %s", protocol));
+        }
+    }
+
+    @Override
+    public String toString() {
+        return this.protocol;
+    }
+
+
 }
